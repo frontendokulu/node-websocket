b58503be27b690bb26b87f9a13d488aaba626658
frontendokulu <frontendokulu@gmail.com>
2020-05-01 15:49:37 +0300

    socket.io kullanılarak server-client arasında bağlantı oluşturuldu

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const socketio = require('socket.io');
+const server = http.createServer((req, res) =>{
+   res.end('o amk nodemon unu global kurunca sorun kalmadı');
+});
+
+server.listen(3000);
+
+//sunucuya gelen istekleri dinlemek için yazıldı.
+//sunucuya her istek geldiğinde aktif bir bağlantı açılacak
+const io = socketio.listen(server);
+
+
+//Kullanıcı 'connection' yaptığında işlemi yakala
+//socket içinde bağlantı yapan kullanıcının bilgileri bulunur.
+io.sockets.on('connection',(socket) =>{
+   console.log('kullanıcı bağlandı');
+
+
+   //socket 'disconnect' olduğunda kullanıcı ayrıldı yazsın :)
+   socket.on('disconnect', () =>{
+      console.log('Kullanıcı ayrıldı')
+   });
+});
