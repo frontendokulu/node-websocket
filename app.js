ae0590e59263e5806f9483de8815e698e7cf8d75
frontendokulu <frontendokulu@gmail.com>
2020-05-01 22:40:03 +0300

    socket.emit ve socket on işlemleri eklendi

--- app.js
+++ app.js
@@ -16,9 +16,20 @@ const io = socketio.listen(server);
 io.sockets.on('connection',(socket) =>{
    console.log('kullanıcı bağlandı');
 
+   //client'dan selamVer mesajı geldi
+   //gelen mesaj karşılandı
+   //console birşeyler yazıldı.
+   socket.on('selamVer',() =>{
+      console.log('client isteği karşılandı');
+   });
+   setTimeout(() =>{
+      socket.emit('merhaba de');
+   },1000);
+
 
    //socket 'disconnect' olduğunda kullanıcı ayrıldı yazsın :)
    socket.on('disconnect', () =>{
       console.log('Kullanıcı ayrıldı')
    });
+
 });
