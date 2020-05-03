262cd06eddb37a52735377be0525b7d902067c0e
frontendokulu <frontendokulu@gmail.com>
2020-05-03 18:35:08 +0300

    room leave işlemleri eklendi

--- room/app.js
+++ room/app.js
@@ -8,14 +8,15 @@ server.listen(3000);
 
 const io = socketio.listen(server);
 io.on('connection', (socket) =>{
+    //joinRoom event'ı
    socket.on('joinRoom',(data) =>{
         // console.log(data);
        socket.join(data.name, () => {
            //odada bulunan kişi sayısını bulur.
-            let count = io.sockets.adapter.rooms[data.name].length;
-           //socket kendi hariç odada bulunan herkese gönderir
-          io.to(data.name).emit('new join',{count});
+           //  let count = io.sockets.adapter.rooms[data.name].length;
 
+           //socket kendi hariç odada bulunan herkese gönderir
+          io.to(data.name).emit('new join', { count: getOnlineCount(io, data) });
           // io odada bulunan herkese gönderir,
           // ilk durumda herkeste görünmez çünkü
           // diğerleri henüz odada değiller :)
@@ -24,4 +25,17 @@ io.on('connection', (socket) =>{
       });
 
    });
+   //leaveRoom event'ı
+    socket.on('leaveRoom', (data)=>{
+        socket.leave(data.name, () =>{
+            io.to(data.name).emit('leavedRoom', { count: getOnlineCount(io, data) });
+            //odadan çıkn client için ayrıca mesaj gönderilmelidir.
+            socket.emit('socket.leaved', {message: 'odadan ayrıldınız'});
+        });
+    });
 });
+
+const getOnlineCount = (io,data) => {
+    const room =  io.sockets.adapter.rooms[data.name];
+    return room ? room.length : 0 ;
+};
